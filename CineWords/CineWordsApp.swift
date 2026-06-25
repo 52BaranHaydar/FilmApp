a49ba743812fcc4047b39e50d6dc1b773edcd870
Baran Haydar <baranhaydar80@gmail.com>
2026-06-25 15:37:15 +0300

    Initial Commit

---
+++ CineWords/CineWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineWordsApp.swift
+//  CineWords
+//
+//  Created by Baran on 25.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CineWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
